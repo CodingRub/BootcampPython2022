fb1171036123ba864d22b9e571ab6a8d14cae788
stra0011 <ruben.strack@etudiant.univ-reims.fr>
2022-01-07 16:48:52 +0100

    Day 1: Ajout jeu Hangman

---
+++ hangman.py
@@ -0,0 +1,107 @@
+from random import *
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+def genRndWord(words: list) -> str:
+    print("Generating the word")
+    return list(choice(words).lower())
+
+def genBlank(word: str) -> list:
+    blank = []
+    for i in range(len(word)):
+        blank.append("_")
+    return blank
+
+def findBlank(word: list, blank: list) -> bool:
+    res = ""
+    if blank == word:
+        res = f"Congratulations! You have found the word {''.join(word)}"
+    else:
+        res = "You have no more lives"
+    return res
+
+def hangmanForLife(stages: list, life: int) -> str:
+    res = ""
+    for i in range(len(stages)):
+        if life == i:
+            res = stages[i]
+    return res
+
+def main(stages = stages) -> None:
+    print("Welcome to the Hangman Game !")
+    print("The game begins !")
+    word = genRndWord(['Computer', 'Hacker', 'Virus', 'Whistleblower', 'Payload', 'Malware', 'Backdoor'])
+    word_copy = word.copy()
+    blank = genBlank(word)
+    life = 5
+    print(''.join(blank))
+    while '_' in blank and life > 0:
+        letter = str(input("Choose a letter ! ").lower())
+        if letter in word:
+            blank[word.index(letter)] = letter
+            word[word.index(letter)] = "_"
+        else:
+            print("Oh no ! The letter is not in the word")
+            life -= 1
+        print(hangmanForLife(stages, life))
+        print(''.join(blank))
+    print(findBlank(word_copy, blank))
+
+
+if __name__ == '__main__':
+    main()
